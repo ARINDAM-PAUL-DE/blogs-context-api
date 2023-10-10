0ac9e22340867808cf2601d0fe6854d193bfc3d9
ARINDAM-PAUL-DE <arindampaul526@gmail.com>
2023-10-11 00:38:07 +0530

    margin top and bottom set

--- blogs-context-starter/src/Components/Blogs.jsx
+++ blogs-context-starter/src/Components/Blogs.jsx
@@ -9,7 +9,7 @@ const Blogs = () => {
     console.log(posts);
 
     return (
-        <div className='w-11/12 max-w-[670px] py-8 flex flex-col gap-y-7 mt-[65px] mb-[70px] justify-center items-center h-screen'>
+        <div className='w-11/12 max-w-[670px] py-8 flex flex-col gap-y-7 mt-[66px] mb-[70px]'>
             {
                 loading ? (<Spinner />) : (
                     posts.length === 0 ?
